4e6073134b336a1ff52a06d03c4121f39b12a41f
joyofdata <raffael.vogler.de@gmail.com>
2013-12-11 15:39:24 +0100

    it's getting real

--- server.r
+++ server.r
@@ -21,7 +21,7 @@ shinyServer(function(input, output) {
 		df
 	})
 	
-	d <- reactive({
+	df_agg_wide <- reactive({
 		if(is.null(df())
 			| input$row == ""
 			| input$col == ""
@@ -45,28 +45,52 @@ shinyServer(function(input, output) {
 			drop=FALSE
 		)
 	})
-	
-	d2 <- reactive({
-		if(is.null(d()))
+		
+	df_agg_long <- reactive({
+		if(is.null(df_agg_wide()))
 			return(NULL)
 			
-		melt(d(),id.vars=input$row,variable.name=input$col,value.name="value")
+		melt(df_agg_wide(),id.vars=input$row,variable.name=input$col,value.name="value")
 	})
 	
-	output$plot <- renderPlot({
-		if(is.null(d2()))
+	output$line_plot <- renderPlot({
+		if(is.null(df_agg_long()))
 			return(NULL)
 	
-		df2 <- d2()
+		df2 <- df_agg_long()
 	
 		n <- names(df2)
 		gg <- ggplot(data=df2,aes_string(x=n[1],y=n[3],color=n[2])) + geom_line()
 		
 		print(gg)
+	},height=1000)
+	
+	output$scatter_plot <- renderPlot({
+		if(is.null(df()))
+			return(NULL)
+	
+		df <- df()
+	
+		n <- names(df)
+		gg <- ggplot(data=df,aes_string(x=input$x,y=input$y,color=input$category)) + geom_point()
+		
+		print(gg)
+	})
+	
+	output$boxplot_plot <- renderPlot({
+		if(is.null(df_agg_long()))
+			return(NULL)
+			
+		df <- df()
+	
+		n <- names(df)
+		gg <- ggplot(data=df,aes_string(y=input$boxplot.value,x=input$boxplot.category,fill=input$boxplot.category)) + geom_boxplot()
+		
+		print(gg)
 	})
 	
 	output$table <- renderDataTable({
-		d()
+		df_agg_wide()
 	})
 	
 	output$structure <- renderPrint({
--- ui.r
+++ ui.r
@@ -1,19 +1,42 @@
 library(shiny)
 
+narrowSidebar <- HTML('<style>.span4 {min-width: 280px; max-width: 280px; }</style>')
+widerMainPanel <- HTML('<style>.span8 {min-width: 1200px; max-width: 1200px; } .shiny-plot-output {min-height:800px; max-height:800px}</style>')
+
 shinyUI(pageWithSidebar(
 	headerPanel("R Big Pivot"),
 	sidebarPanel(
+	tags$head(narrowSidebar),
 		fileInput("file1", "TSV:"),
-		textInput("row", "Row (Y)", "time"),
-		textInput("col", "Col (X)", "tra_meas"),
-		textInput("val", "Val", "value"),
-		selectInput("fun", "Fun", list("mean"="mean","sum"="sum","count"="length")),
+		tabsetPanel(
+			tabPanel("lineplot",
+				h4("line plot:"),
+				textInput("row", "Row (X)", "time"),
+				textInput("col", "Column (Category)", "tra_meas"),
+				textInput("val", "Value (Y)", "value"),
+				selectInput("fun", "Aggregation", list("mean"="mean","sum"="sum","count"="length")),
+				HTML("<textarea name='filter.line.plot' rows=10 cols=10></textarea>")
+			),
+			tabPanel("scatterplot",
+				h4("scatter plot:"),
+				textInput("x","X"),
+				textInput("y","Y"),
+				textInput("category","Category")
+			),
+			tabPanel("boxplot",
+				h4("box plot"),
+				textInput("boxplot.category","Category"),
+				textInput("boxplot.value","Value")
+			)
+		),
 		submitButton("do it!")
 	),
-	
 	mainPanel(
+	tags$head(widerMainPanel),
 		tabsetPanel(
-			tabPanel("plot", plotOutput("plot")),
+			tabPanel("line plot", plotOutput("line_plot")),
+			tabPanel("scatter plot", plotOutput("scatter_plot")),
+			tabPanel("boxplot plot", plotOutput("boxplot_plot")),
 			tabPanel("table", dataTableOutput("table")),
 			tabPanel("structure", verbatimTextOutput("structure"))
 		)
