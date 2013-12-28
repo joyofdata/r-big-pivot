3116f18d4c4ac0f535711ce8d5ccc73307bf7633
joyofdata <raffael.vogler.de@gmail.com>
2013-12-28 14:52:40 +0100

    added form for adjusting display spacings

--- ui.r
+++ ui.r
@@ -1,12 +1,19 @@
 library(shiny)
 
-narrowSidebar <- HTML('<style>.span4 {min-width: 280px; max-width: 280px; } textarea {font-family: Courier; font-size: 8pt}</style>')
-widerMainPanel <- HTML('<style>.span8 {min-width: 1200px; max-width: 1200px; } .shiny-plot-output {min-height:800px; max-height:800px}</style>')
-
+narrowSidebar <- HTML('<style>.span4 {min-width: 300px; max-width: 300px; } textarea {font-family: Courier; font-size: 8pt}</style>')
+widerMainPanel <- HTML('<style>.span8 {min-width: 1500px; max-width: 1500px; } .shiny-plot-output {min-height:1000px; max-height:1000px}</style>')
+flexibleSizes <- HTML("w1:<input id='w1' value='280'> - w2:<input id='w2' value='1200'> - h:<input id='h' value='800'> - <input type='button' value='set' id='set'>
+						<style>#w1,#w2,#h{width:30px}</style>
+						<script>
+							$('#set').click(function(){$('.span4').css({'min-width':$('#w1').val()+'px','max-width':$('#w1').val()+'px'})})
+							$('#set').click(function(){$('.span8').css({'min-width':$('#w2').val()+'px','max-width':$('#w2').val()+'px'})})
+							$('#set').click(function(){$('.shiny-plot-output').css({'min-height':$('#h').val()+'px','max-height':$('#h').val()+'px'})})
+						</script>")
 shinyUI(pageWithSidebar(
 	headerPanel("R Big Pivot"),
 	sidebarPanel(
 	tags$head(narrowSidebar),
+		flexibleSizes,
 		fileInput("file1", "TSV:"),
 		tabsetPanel(
 			tabPanel("plot",
